1dbb78b6c825ecb4fd9aa8c72e4f41177b9dd584
adanSmith01 <smith.paredes.13@gmail.com>
2025-09-14 14:39:15 -0300

    adan, viewmodels, agrego el viewmodel para mostrar la lista de empleados

---
+++ WebApplication1/ViewModels/MostrarUsuariosVM.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.ViewModels
+{
+    public class MostrarUsuariosVM
+    {
+        public List<Usuario> usuarios { get; }
+
+        public MostrarUsuariosVM(List<Usuario> usuarios)
+        {
+            this.usuarios = usuarios;
+        }
+    }
+}
